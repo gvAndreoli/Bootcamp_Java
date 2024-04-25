6daeaeb81d3b1d52201b715de24c3c65ce5aeb55
gvAndreoli <gabrielvidalandreoli@gmail.com>
2024-04-25 10:44:05 -0300

    Criando o primeiro projeto em java no vs code

---
+++ projeto_inicial/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("oi %d%n", i);
+        }
+    }
+}
