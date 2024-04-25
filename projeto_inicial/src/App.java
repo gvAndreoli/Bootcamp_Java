4bb422b1b4138d3bfcf9a34a757bb9738c89bcd8
gvAndreoli <gabrielvidalandreoli@gmail.com>
2024-04-25 12:40:52 -0300

    troquei de IDE

--- projeto_inicial/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.printf("oi %d%n", i);
-        }
-    }
-}
