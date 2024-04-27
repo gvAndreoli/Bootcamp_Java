f4cb8a54abbdc2453696e93fead7df2939828700
gvAndreoli <gabrielvidalandreoli@gmail.com>
2024-04-27 20:57:17 -0300

    palavras reservadas

---
+++ Revendo_o_basico/src/edu/gabriel/palavrasReservadas/PalavrasReservadas.java
@@ -0,0 +1,12 @@
+package edu.gabriel.palavrasReservadas;
+
+public class PalavrasReservadas {
+    // nomes de tipos primitivos
+    // estruturas de repetição: while, for
+    // estruturas condicionais: if, else
+    // imports: package, import etc
+    // visibilidade: public, private, protected
+    // retorno: return
+    // relacionados a classe: class, extend, abstract, implements, static, new, super, this
+    // final
+}
