fae3affce2e921325999d3131e1f2375e09b28e5
gvAndreoli <gabrielvidalandreoli@gmail.com>
2024-04-27 18:03:48 -0300

    tipos em java

---
+++ Revendo_o_basico/src/edu/gabriel/tipos/TiposDoJava.java
@@ -0,0 +1,18 @@
+package edu.gabriel.tipos;
+
+import java.time.chrono.MinguoDate;
+
+public class TiposDoJava {
+    public static void main(String[] args) {
+        // os tipos primitivos do java são char, int, float, double, short, long, byte, boolean
+        // porem na grande maioria dos casos a gente só usa, int, double e boolean
+        // também há os tipos não primitivos, como String e outras classes
+
+     int idade = 19;
+     double altura = 1.72;
+     boolean solteiro = true;
+     String nome = "Gabriel";
+
+     final double CONSTANTE = 3.14;
+    }
+}
